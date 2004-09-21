aa1bfa7bc8b43ea2d60b0140add80540194c5e38
sstratos <sstratos>
2004-09-21 18:50:29 +0000

    *** empty log message ***

---
+++ lib/HelpWindow.cpp
@@ -0,0 +1,43 @@
+/*
+ *  Qtstalker stock charter
+ * 
+ *  Copyright (C) 2001-2004 Stefan S. Stratigakos
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, 
+ *  USA.
+ */
+
+#include "HelpWindow.h"
+#include <qlayout.h>
+
+HelpWindow::HelpWindow (QWidget *, QString path) : QDialog (0, "HelpWindow", FALSE, WDestructiveClose)
+{
+  resize(500, 350);
+
+  QVBoxLayout *vbox = new QVBoxLayout (this);
+  vbox->setSpacing(5);
+  vbox->setMargin(5);
+
+  text = new QTextBrowser(this);
+  text->setSource(path);
+  vbox->addWidget (text);
+  
+  setCaption("Qtstalker: " + text->documentTitle());
+}
+
+HelpWindow::~HelpWindow ()
+{
+}
+
