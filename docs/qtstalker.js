61d1d00c7bd646cbbf58992313675fedc01dcc22
xley <xley>
2009-09-04 11:31:55 +0000

    Add a simple banner to every page warning that docs are out-of-date. Next step is to delete some not needed pages and trim down the rest. When a page is ready, then remove the <div id="warnDoc"> element.

--- docs/qtstalker.js
+++ docs/qtstalker.js
@@ -1,4 +1,6 @@
 window.onload = function() {
  var oDiv = document.getElementById("version");
  oDiv.innerHTML = "Version 0.37-dev<br />(other <a href='faq.html#versions'>versions</a>)";
+ var oDiv2 = document.getElementById("warnDoc");
+ oDiv2.innerHTML = "<strong>Warning:</strong><br />This is documentation for the previous version, and is not yet updated.";
 }
