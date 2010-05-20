504fe557bb868f62a915ac770df0d4e6d43a1e08
xley <xley>
2010-05-20 22:38:03 +0000

    Return some warnings, and fine-tune the notice message.

--- docs/qtstalker.js
+++ docs/qtstalker.js
@@ -3,6 +3,6 @@ window.onload = function() {
  oDiv.innerHTML = "Version 0.37-dev<br />(other <a href='faq.html#versions'>versions</a>)";
  var oDiv2 = document.getElementById("warnDoc");
  if (oDiv2 != null) {
-  oDiv2.innerHTML = "<strong>Warning:</strong><br />This is documentation for the previous version, and is not yet updated.";
+  oDiv2.innerHTML = "<strong>Warning:</strong><br />This is document needs some updates.";
  }
 }
