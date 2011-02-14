14170d4449ffaf307109b1558192a6e677d15111
sstratos <sstratos>
2011-02-14 18:15:50 +0000

    *** empty log message ***

---
+++ plugins/script/STDDEV/STDDEV.cpp
@@ -0,0 +1,139 @@
+/*
+ *  Qtstalker stock charter
+ *
+ *  Copyright (C) 2001-2010 Stefan S. Stratigakos
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ */
+
+#include "STDDEV.h"
+#include "Curve.h"
+#include "ta_libc.h"
+#include "Globals.h"
+
+#include <QtDebug>
+
+STDDEV::STDDEV ()
+{
+  _plugin = "STDDEV";
+}
+
+int STDDEV::command (Command *command)
+{
+  // PARMS:
+  // NAME
+  // INPUT
+  // PERIOD
+  // DEVIATIONS
+
+  Indicator *i = command->indicator();
+  if (! i)
+  {
+    qDebug() << _plugin << "::command: no indicator";
+    return 1;
+  }
+
+  QString name = command->parm("NAME");
+  Curve *line = i->line(name);
+  if (line)
+  {
+    qDebug() << _plugin << "::command: duplicate NAME" << name;
+    return 1;
+  }
+
+  Curve *in = i->line(command->parm("INPUT"));
+  if (! in)
+  {
+    qDebug() << _plugin << "::command: INPUT missing" << command->parm("INPUT");
+    return 1;
+  }
+
+  bool ok;
+  int period = command->parm("PERIOD").toInt(&ok);
+  if (! ok)
+  {
+    qDebug() << _plugin << "::command: invalid PERIOD" << command->parm("PERIOD");
+    return 1;
+  }
+
+  double dev = command->parm("DEVIATIONS").toDouble(&ok);
+  if (! ok)
+  {
+    qDebug() << _plugin << "::command: invalid DEVIATIONS" << command->parm("DEVIATIONS");
+    return 1;
+  }
+
+  if (in->count() < period)
+    return 1;
+
+  TA_Real input[in->count()];
+  TA_Real out[in->count()];
+  TA_Integer outBeg;
+  TA_Integer outNb;
+
+  QList<int> keys;
+  in->keys(keys);
+
+  int loop = 0;
+  for (; loop < keys.count(); loop++)
+  {
+    CurveBar *bar = in->bar(keys.at(loop));
+    input[loop] = (TA_Real) bar->data();
+  }
+
+  TA_RetCode rc = TA_STDDEV(0,
+                            keys.count() - 1,
+                            &input[0],
+                            period,
+                            dev,
+                            &outBeg,
+                            &outNb,
+                            &out[0]);
+  
+  if (rc != TA_SUCCESS)
+  {
+    qDebug() << _plugin << "::command: TA-Lib error" << rc;
+    return 1;
+  }
+
+  line = new Curve;
+
+  int keyLoop = keys.count() - 1;
+  int outLoop = outNb - 1;
+  while (keyLoop > -1 && outLoop > -1)
+  {
+    line->setBar(keys.at(keyLoop), new CurveBar(out[outLoop]));
+    keyLoop--;
+    outLoop--;
+  }
+
+  line->setLabel(name);
+  i->setLine(name, line);
+
+  command->setReturnCode("0");
+
+  return 0;
+}
+
+//*************************************************************
+//*************************************************************
+//*************************************************************
+
+ScriptPlugin * createScriptPlugin ()
+{
+  STDDEV *o = new STDDEV;
+  return ((ScriptPlugin *) o);
+}
