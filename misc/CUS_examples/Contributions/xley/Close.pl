6ad5466049389afad8f798226c1715d290635fb6
xley <xley>
2011-03-30 00:08:27 +0000

    Add a simple Close indicator example, with example debug statements.

---
+++ misc/CUS_examples/Contributions/xley/Close.pl
@@ -0,0 +1,24 @@
+# qtstalker Close indicator
+
+use File::Basename;
+
+$| = 1;
+my $DEBUG = 0; # 0..1
+my $scriptName = basename($0);
+if ($DEBUG) { doDebug('Start'); }
+
+if ($DEBUG) { doDebug('Get close_0'); }
+print STDOUT 'PLUGIN=DOHLCVI,METHOD=C,NAME=close_0';
+$rc = <STDIN>; chomp($rc); if ($rc ne '0') { exit; }
+
+# set the plot style
+if ($DEBUG) { doDebug('Do Plot'); }
+print STDOUT 'PLUGIN=INDICATOR_PLOT_ALL,NAME=close_0,STYLE=Line,COLOR=yellow,Z=1';
+$rc = <STDIN>; chomp($rc); if ($rc ne '0') { exit; }
+
+if ($DEBUG) { doDebug('End'); }
+
+sub doDebug {
+  my ($msg) = @_;
+  print STDERR "DEBUG: $scriptName: $msg\n";
+}
